6b2bedf40aed0f79dec2810252e661c65853b082
Tharindu Supasan Aththanayaka <tharindu.supasan@gmail.com>
2024-11-30 10:38:05 +0530

    C++ Basic Arithmetic Operations
    This C++ program demonstrates basic arithmetic operations between two integers. The program prompts the user to enter two integers (x and y) and then performs the following operations:
    
    Addition (x + y)
    Subtraction (x - y)
    Multiplication (x * y)
    Modulus (x % y, which gives the remainder of the division)
    Division (x / y, which gives the quotient)
    
    The program displays the result of each operation, helping to reinforce the understanding of arithmetic operators in C++ and how they are applied to integer values. This exercise helped improve my familiarity with performing basic mathematical calculations in a program.

---
+++ 3.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter the first integer : ";
+    cin>>x;
+    cout<<"Enter the second integer : ";
+    cin>>y;
+
+    cout<<"x + y = "<<(x+y)<<endl;
+    cout<<"x - y = "<<(x-y)<<endl;
+    cout<<"x * y = "<<(x*y)<<endl;
+    cout<<"x % y = "<<(x%y)<<endl;
+    cout<<"x / y = "<<(x/y)<<endl;
+    return 0;
+
+}
